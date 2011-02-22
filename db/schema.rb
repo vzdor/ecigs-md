a24ebe69215559d07211eed5b8b49a7039296f5a
Vladimir Zdorovenco <breakunit@gmail.com>
2011-02-22 14:13:16 +0200

    add is_admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110222091908) do
+ActiveRecord::Schema.define(:version => 20110222120136) do
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20110222091908) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
