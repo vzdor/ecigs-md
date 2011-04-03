86eb02d88bda3437b15c8565fee51305007da77e
Vladimir Zdorovenco <breakunit@gmail.com>
2011-04-03 14:40:17 +0300

    product variations js validation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function verifyVariationsQuantity()
+{
+    var inputs = $$("div.variations input[type=\"text\"]");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value > 1)
+            return true
+    }
+    $("variations-quantity-notice").show(); // .highlight();
+    return false;
+}
