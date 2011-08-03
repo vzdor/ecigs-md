62dbb4ba7492b0c3844de091d0daaa612c772573
Vladimir Zdorovenco <breakunit@gmail.com>
2011-08-03 19:31:39 +0300

    do not encdoe wiki urls

--- lib/red_cloth_wiki_extension.rb
+++ lib/red_cloth_wiki_extension.rb
@@ -1,7 +1,8 @@
 module RedClothWikiExtension
   def wiki(text)
     text.gsub!(/\[\[((?:.(?!:))*)\]\]/) do |m|
-      "<a href=\"/wiki_pages/#{URI::encode(WikiPage.slug($1))}\">#{$1}</a>"
+      # "<a href=\"/wiki_pages/#{URI::encode(WikiPage.slug($1))}\">#{$1}</a>"
+      "<a href=\"/wiki_pages/#{WikiPage.slug($1)}\">#{$1}</a>"
     end
   end
 end
