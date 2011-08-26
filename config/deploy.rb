2f087ecd49a6364365eda6cf55b5d14cce755949
Vladimir Zdorovenco <breakunit@gmail.com>
2011-08-26 11:52:20 +0300

    rm generated js/css bundles on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,8 @@ role :db,  "gw", :primary => true # This is where Rails migrations will run
 
 after 'deploy:symlink', 'deploy:symlink_config'
 
+after 'deploy:symlink', 'deploy:rm_css_js_bundles'
+
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
@@ -23,5 +25,10 @@ namespace :deploy do
   end
   task :symlink_config do
     run "ln -nfs #{shared_path}/ecigs.yml #{release_path}/config/configatron"
+  end
+
+  task :rm_css_js_bundles do
+    run "rm -f #{shared_path}/system/all.js"
+    run "rm -f #{shared_path}/system/all.css"
   end
 end
